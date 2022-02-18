cfa8526665c2f13001d8cab4d358ad15170782b3
yoctoMNS <yocto.mns@gmail.com>
2022-02-18 21:31:56 +0900

    EP39

--- src/game/state/GameState.java
+++ src/game/state/GameState.java
@@ -2,24 +2,41 @@ package game.state;
 
 import controller.NPCController;
 import controller.PlayerController;
+import core.Condition;
 import core.Size;
 import entity.NPC;
 import entity.Player;
 import entity.SelectionCircle;
-import entity.humanoid.Humanoid;
 import entity.humanoid.effect.Isolated;
 import entity.humanoid.effect.Sick;
 import game.ui.UIGameTime;
 import game.ui.UISicknessStatistics;
 import input.Input;
 import map.GameMap;
+import map.ui.Alignment;
+import map.ui.UIText;
+import map.ui.VerticalContainer;
+
+import java.util.List;
 
 public class GameState extends State {
+    private List<Condition> victoryConditions;
+    private List<Condition> defeatConditions;
+    private boolean playing;
+
     public GameState(Size windowSize, Input input) {
         super(windowSize, input);
         this.gameMap = new GameMap(new Size(20, 20), spriteLibrary);
+        this.playing = true;
+
         initializeCharacters();
         initializeUI(windowSize);
+        initializeConditions();
+    }
+
+    private void initializeConditions() {
+        victoryConditions = List.of(() -> getNumberOfSick() == 0);
+        defeatConditions = List.of(() -> (float)getNumberOfSick() / getNumberOfNPCs() > 0.25);
     }
 
     private void initializeUI(Size windowSize) {
@@ -37,7 +54,7 @@ public class GameState extends State {
         gameObjects.add(circle);
 
         initializeNPCs(200);
-        makeNumberOfNPCsSick(10);
+        makeNumberOfNPCsSick(0);
     }
 
     private void makeNumberOfNPCsSick(int number) {
@@ -54,21 +71,56 @@ public class GameState extends State {
         }
     }
 
+    @Override
+    public void update() {
+        super.update();
+
+        if (victoryConditions.stream().allMatch(Condition::isMet)) {
+            win();
+        }
+
+        if (defeatConditions.stream().allMatch(Condition::isMet)) {
+            lose();
+        }
+    }
+
+    private void win() {
+        playing = false;
+
+        VerticalContainer winContainer = new VerticalContainer(camera.getSize());
+        winContainer.setAlignment(new Alignment(Alignment.Position.CENTER, Alignment.Position.CENTER));
+        winContainer.addUIComponent(new UIText("VICTORY"));
+        uiContainers.add(winContainer);
+    }
+
+    private void lose() {
+        playing = false;
+
+        VerticalContainer loseContainer = new VerticalContainer(camera.getSize());
+        loseContainer.setAlignment(new Alignment(Alignment.Position.CENTER, Alignment.Position.CENTER));
+        loseContainer.addUIComponent(new UIText("DEFEAT"));
+        uiContainers.add(loseContainer);
+    }
+
     public long getNumberOfSick() {
-         return getGameObjectsOfClass(Humanoid.class).stream()
-                .filter(humanoid -> humanoid.isAffectedBy(Sick.class) && !humanoid.isAffectedBy(Isolated.class))
+         return getGameObjectsOfClass(NPC.class).stream()
+                .filter(npc -> npc.isAffectedBy(Sick.class) && !npc.isAffectedBy(Isolated.class))
                 .count();
     }
 
     public long getNumberOfIsolated() {
-        return getGameObjectsOfClass(Humanoid.class).stream()
-                .filter(humanoid -> humanoid.isAffectedBy(Sick.class) && humanoid.isAffectedBy(Isolated.class))
+        return getGameObjectsOfClass(NPC.class).stream()
+                .filter(npc -> npc.isAffectedBy(Sick.class) && npc.isAffectedBy(Isolated.class))
                 .count();
     }
 
     public long getNumberOfHealthy() {
-        return getGameObjectsOfClass(Humanoid.class).stream()
-                .filter(humanoid -> !humanoid.isAffectedBy(Sick.class))
+        return getGameObjectsOfClass(NPC.class).stream()
+                .filter(npc -> !npc.isAffectedBy(Sick.class))
                 .count();
+    }
+
+    public long getNumberOfNPCs() {
+        return getGameObjectsOfClass(NPC.class).size();
     }
 }
